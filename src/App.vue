1d4b80885cee47c67228295d65cf260862c3387c
gabriel <gabrielcoutinho2011@gmail.com>
2024-03-26 16:55:23 -0300

    feature - Adicionando o header, routerview e footer

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,24 @@
 <template>
   <v-app>
-    <v-main >
-      <v-container >
-        <TabelaRegistroRedzone :headers="headers" :desserts="desserts" />
-
-      </v-container>
-
-
+    <v-main>
+      <div>
+        <HeaderComponent />
+      </div>
+      <div>
+        <RouterView />
+      </div>
     </v-main>
+    <v-footer fixed style="padding: 0 !important;">
+      <FooterComponent />
+    </v-footer>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import TabelaRegistroRedzone from './components/TabelaRegistroRedzone.vue';
-const headers = [
-  { title: 'Id', value: 'id' },
-  { title: 'Redzone', value: 'redzone' },
-  { title: 'Dia', value: 'dia' },
-  { title: 'Lotação atual', value: 'lotacaoAtual' },
-]
-const desserts = [
-  {
-    id: 1,
-    redzone: 'Almoxarifado',
-    lotacaoAtual: 2,
-    dia: 'c. 2580–2560 BC',
-    lotacaoMaxima: 10
-  },
-  {
-    id: 2,
-    redzone: 'Almoxarifado',
-    lotacaoAtual: 14,
-    dia: 'c. 2570 BC',
-    lotacaoMaxima: 10
-  },
-  {
-    id: 3,
-    redzone: 'Almoxarifado',
-    lotacaoAtual: 12,
-    dia: 'c. 2590 BC',
-    lotacaoMaxima: 10
-  },
-  {
-    id: 4,
-    redzone: 'Cozinha',
-    lotacaoAtual: 7,
-    dia: 'c. 2600 BC',
-    lotacaoMaxima: 8
-  },
-  {
-    id: 5,
-    redzone: 'Cozinha',
-    lotacaoAtual: 12,
-    dia: 'c. 200 CE',
-    lotacaoMaxima: 8
-  },
-  {
-    id: 6,
-    redzone: 'Cozinha',
-    lotacaoAtual: 19,
-    dia: 'c. 200 CE',
-    lotacaoMaxima: 8
-  },
-  {
-    id: 7,
-    redzone: 'Caldeira',
-    lotacaoAtual: 5,
-    dia: 'c. 200 CE',
-    lotacaoMaxima: 13
-  },
-  {
-    id: 8,
-    redzone: 'Caldeira',
-    lotacaoAtual: 3,
-    dia: 'c. 200 CE',
-    lotacaoMaxima: 13
-  },
-]
+import { RouterView } from 'vue-router';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+
 
 </script>
