1530a3f081efcc55dc688959dc417af9543c70a3
gabriel <gabrielcoutinho2011@gmail.com>
2024-03-26 16:57:00 -0300

    feature - Criando a rota de registro

---
+++ src/router/RegistroRoute.ts
@@ -0,0 +1,13 @@
+import RegistroRedzoneView from '../views/RegistroRedzoneView.vue'
+
+const loginRoute = {
+  path: '/',
+  name: 'registro',
+  meta: {
+    title: 'registro'
+  },
+  component: RegistroRedzoneView 
+
+};
+
+export default loginRoute;
