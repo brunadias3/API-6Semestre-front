9e73cd401db901fe17da26c40f3e6def2892528d
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-03-26 11:43:18 -0300

    feature - importação dos componentes

--- components.d.ts
+++ components.d.ts
@@ -8,5 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    TabelaRegistro: typeof import('./src/components/TabelaRegistro.vue')['default']
+    TabelaRegistroRedzone: typeof import('./src/components/TabelaRegistroRedzone.vue')['default']
   }
 }
