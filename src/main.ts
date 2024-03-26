9d1b2172ce22887d552755b6778a6a54927b9461
gabriel <gabrielcoutinho2011@gmail.com>
2024-03-26 16:55:33 -0300

    feature - Adicionando o router

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,11 @@ import { registerPlugins } from '@/plugins'
 
 // Components
 import App from './App.vue'
-
 // Composables
 import { createApp } from 'vue'
+import router from './router'
 
 const app = createApp(App)
-
+app.use(router)
 registerPlugins(app)
-
 app.mount('#app')
