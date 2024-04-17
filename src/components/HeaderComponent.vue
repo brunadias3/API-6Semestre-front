cbc1f287850f4030bd0f2eec01e82333ed59c97d
gabriel <gabrielcoutinho2011@gmail.com>
2024-04-17 12:58:49 -0300

    feature: Adicionando rota de departamento

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,19 +1,19 @@
 <template>
     <v-div bg-color="#015280">
-        <v-breadcrumbs class="justify-space-between bg-light-blue-darken-4" >
-            <img src="../assets/logoAltave-removebg-preview.png" alt="logo-altave">
+        <v-breadcrumbs class="justify-space-between bg-light-blue-darken-4">
+            <img class="cursor-pointer" src="../assets/logoAltave-removebg-preview.png"
+                @click="$router.push({ name: 'registro' })" alt="logo-altave">
             <div class="text-center">
                 <v-menu v-model="menu" :close-on-content-click="false" location="end">
-                    <!-- <template v-slot:activator="{ props }">
+                    <template v-slot:activator="{ props }">
                         <v-btn color="light-blue-darken-1" v-bind="props">
                             Nome usuário
                         </v-btn>
-                    </template> -->
+                    </template>
 
                     <v-card min-width="300">
-                        <v-list >
-                            <v-list-item @click="logzada"
-                                subtitle="Founder of Vuetify" title="John Leider">
+                        <v-list>
+                            <v-list-item @click="logzada" subtitle="Founder of Vuetify" title="John Leider">
                             </v-list-item>
                         </v-list>
 
@@ -27,16 +27,16 @@
                                 </div>
                             </v-list-item>
                             <v-divider></v-divider>
-                            <v-list-item @click="logzada">
+                            <v-list-item @click="enviarRota('departamento')">
                                 <div class="d-flex ga-3">
                                     <v-icon icon="fa-book" color="#015280" />
                                     <div class="text-overline">Departamentos</div>
                                 </div>
                             </v-list-item>
-                            
+
                             <v-divider></v-divider>
 
-                            <v-list-item @click="logzada">
+                            <v-list-item @click="enviarRota('registro')">
                                 <div class="d-flex ga-3">
                                     <v-icon icon="mdi mdi-circle-off-outline" color="#015280" />
                                     <div class="text-overline">Redzone</div>
@@ -60,13 +60,18 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-const fav = ref(true)
 const menu = ref(false);
-const message = ref(true);
-const hints = ref(true)
+const router = useRouter();
+
 const logzada = () => {
     console.log('LOGZADAA!!!');
-
 }
+
+const enviarRota = (rota: string) => {
+    router.push({ name: rota });
+    menu.value = false;
+}
+
 </script>
