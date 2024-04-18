3f72aa883cf0af306be2a77de3e3f70635c77b4c
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-04-18 11:21:57 -0300

    feature: Adicionando nova tipagem do departamento

--- src/interfaces/IDepartamento.ts
+++ src/interfaces/IDepartamento.ts
@@ -1,5 +1,28 @@
+
+interface IUsuario {
+    id_usuario: number,
+    nome_usuario: string,
+    email: string,
+    matricula_empresa: string,
+    senha: string,
+    tipo_usuario: string,
+    delete_at: null,
+    create_at: string,
+    update_at: string,
+    deleted: boolean,
+    desativado: boolean
+}
+interface IRegistroDepartamento {
+    nome_departamento?: string;
+    id_departamento?: number;
+    delete_at: null,
+    create_at: string,
+    update_at: string,
+    deleted: boolean,
+    desativado: boolean
+    responsavel_id: IUsuario
+}
+
 export default interface IDepartamento {
-    id: number;
-    departamento: string;
-    data: string;
+    registro: IRegistroDepartamento[]
 }
