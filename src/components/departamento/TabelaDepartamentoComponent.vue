018539eecfbe14e9536ef7be208d710c69239fc3
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-04-18 11:24:52 -0300

    feature: Alterando os dados da tabela para puxar os dados do departamento

--- src/components/departamento/TabelaDepartamentoComponent.vue
+++ src/components/departamento/TabelaDepartamentoComponent.vue
@@ -3,9 +3,8 @@
         <v-main>
             <v-container>
                 <TitleComponent title="Gerenciamento de Departamentos" />
-                <TabelaComponent titulo="Departamentos" :headers="headers" :desserts="registroRedzone.dadosRedzone"
-                    adicionar="Criar departamento" rota="criarDepartamento" rotaEditar="editarDepartamento"
-                    :desativar="desativar" />
+                <TabelaComponent titulo="Departamentos" :headers="headers" :dessertsDepartamento="departamentoStoreDados.departamento"
+                    adicionar="Criar departamento" rota="criarDepartamento" rotaEditar="editarDepartamento" />
             </v-container>
         </v-main>
 
@@ -16,33 +15,32 @@
 import { onMounted } from 'vue';
 import TabelaComponent from '../TabelaComponent.vue';
 import TitleComponent from '../TitleComponent.vue';
-import { departamentoStore, registroRedzoneStore } from '../../stores';
+import { departamentoStore } from '../../stores';
 
-const registroRedzone = registroRedzoneStore()
 const departamentoStoreDados = departamentoStore();
 
 const headers = [
-    { title: 'Id', value: 'id' },
-    { title: 'Dia', value: 'data' },
-    { title: 'Lotação atual', value: 'lotacaoAtual' },
-    { title: 'Entrada/Saida', value: 'entrada' },
+    { title: 'Id do departamento', value: 'id_departamento' },
+    { title: 'Nome do departamento', value: 'nome_usuario' },
+    { title: 'Criado em', value: 'create_at' },
+    { title: 'Nome do Responsável', value: 'nome_responsavel' },
     { title: 'Desativar', value: 'desativar' },
     { title: 'Editar', value: 'editar' }
 
 
 ]
-const desativar = (id: number) => {
-    try {
-        departamentoStoreDados.desativarDepartamento(id)
-    } catch (error) {
-        console.log(error);
+// const desativar = (id: number) => {
+//     try {
+//         departamentoStoreDados.desativarDepartamento(id)
+//     } catch (error) {
+//         console.log(error);
 
-    }
-    console.log(id);
+//     }
+//     console.log(id);
 
-}
+// }
 const pegarDados = async () => {
-    await registroRedzone.pegarHistoricoRedZone()
+    await departamentoStoreDados.getDepartamento();
 }
 
 onMounted(() => {
