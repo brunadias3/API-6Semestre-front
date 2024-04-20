124833b554f5f6d4666ab27835603e522e0967a5
gabriel <gabrielcoutinho2011@gmail.com>
2024-04-20 19:39:52 -0300

    feature: Trocando variavel de verificação

--- components.d.ts
+++ components.d.ts
@@ -9,5 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TabelaDepartamentoComponent: typeof import('./src/components/departamento/TabelaDepartamentoComponent.vue')['default']
   }
 }
