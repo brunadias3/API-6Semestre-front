bc75af6b35fca310b7326666704572e5d9d02c08
Bruna <brunadias_30@hotmail.com>
2024-04-22 20:31:25 -0300

    fix: arrumei tipagem da redzone e criei formatters de data

--- src/types/IRedzone.ts
+++ src/types/IRedzone.ts
@@ -3,10 +3,11 @@ import { Usuario } from "./IUsuario";
 
 export interface Redzone {
   id_redzone?: number,
-  nome_redzone: string,
-  responsavel_id: Usuario,
-  camera: string,
-  capacidade_maxima: number,
-  id_departamento: Departamento
-  status: boolean
+  nome_redzone?: string,
+  responsavel_id?: Usuario,
+  camera?: string,
+  capacidade_maxima?: number,
+  id_departamento?: Departamento
+  status?: boolean
+  delete_at?: any
 }
