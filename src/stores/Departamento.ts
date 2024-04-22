b53475ca033f291f7907f1d1e41b22f5bdd64384
gabriel <gabrielcoutinho2011@gmail.com>
2024-04-22 17:21:31 -0300

    feature: Alterando os body de criar e editar e criando a função de edição

--- src/stores/Departamento.ts
+++ src/stores/Departamento.ts
@@ -1,24 +1,23 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import { deleteRequest, getRequest, postRequest } from "../utils/services/axios";
+import { deleteRequest, getRequest, postRequest, putRequest } from "../utils/services/axios";
 import IDepartamento from "../interfaces/IDepartamento";
 import INovoDepartamento from "../interfaces/INovoDepartamento";
+import IEditarDepartamento from "../interfaces/IEditarDepartamento";
 
 
 const departamentoStore = defineStore('departamento', () => {
     const departamento = ref<IDepartamento[]>([])
     const errorCatch = ref<unknown>(undefined);
     const novoDepartamento = ref<INovoDepartamento>({} as INovoDepartamento)
-    const editarDepartamento = ref<INovoDepartamento>({} as INovoDepartamento)
+    const editarDepartamento = ref<IEditarDepartamento>({} as IEditarDepartamento)
 
 
     const criarDepartamento = async (departamento: INovoDepartamento) => {
         try {
             const body = {
                 nome_departamento: departamento.nome_departamento,
-                responsavel_id: {
-                    id_usuario: departamento.responsavel_id
-                }
+                responsavel_id: departamento.responsavel_id
             };
             const response = await postRequest('departamentos', body);
 
@@ -32,7 +31,7 @@ const departamentoStore = defineStore('departamento', () => {
         try {
             const response = await getRequest('departamentos')
 
-            departamento.value = response.data.reverse()
+            departamento.value = response.data
         } catch (error) {
             errorCatch.value = error;
         }
@@ -48,12 +47,29 @@ const departamentoStore = defineStore('departamento', () => {
         }
     }
 
+    const alterarDepartamento = async (id: string | string[], departamento: IEditarDepartamento) => {
+        try {
+            const body = {
+                id_departamento: id,
+                nome_departamento: departamento.nome_departamento,
+                responsavel_id: departamento.responsavel_id
+
+            }
+            const response = await putRequest(`departamentos/${id}`, body)
+
+            return response;
+        } catch (error) {
+            errorCatch.value = error;
+        }
+    }
+
+
     const desativarOuAtivarDepartamento = async (id: string) => {
         try {
             const response = await deleteRequest(`departamentos/${id}`)
 
-           departamento.value = response.data.reverse()
-            
+            departamento.value = response.data.reverse()
+
         } catch (error) {
             errorCatch.value = error;
         }
@@ -66,7 +82,8 @@ const departamentoStore = defineStore('departamento', () => {
         getDepartamento,
         criarDepartamento,
         desativarOuAtivarDepartamento,
-        getDepartamentoById
+        getDepartamentoById,
+        alterarDepartamento
     }
 })
 
