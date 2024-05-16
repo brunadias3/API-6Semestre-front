837d8eb8c9d4e309784dcb1200fd3216d4344461
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-05-16 11:21:26 -0300

    feature: Exportando a store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,3 +2,4 @@ export { default as registroRedzoneStore } from './RegistroRedzone'
 export { default as RedzoneStore } from './Redzone'
 export { default as departamentoStore } from './Departamento'
 export { default as UsuarioStore } from './Usuario';
+export { default as LoginStore} from './Login';
