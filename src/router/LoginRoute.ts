ac7223d70bde033081143a120581f3fe40a2216b
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-05-16 11:19:09 -0300

    feature: Criando a rota de login

---
+++ src/router/LoginRoute.ts
@@ -0,0 +1,13 @@
+import LoginView from "../views/Login/LoginView.vue"
+
+const LoginRoute = {
+  path: '/',
+  name: 'login',
+  meta: {
+    title: 'login'
+  },
+  component: LoginView
+
+};
+
+export default LoginRoute;
