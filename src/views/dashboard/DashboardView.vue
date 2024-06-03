81a02836a74394a71076599b61c57135a10f5f66
Bruna <brunadias_30@hotmail.com>
2024-06-03 20:22:27 -0300

    feature: conectando 8 card e graficos com o backend

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,12 +1,21 @@
 <template>
+  <v-overlay :model-value="loading" class="align-center justify-center">
+    <v-progress-circular
+      v-model="loading"
+      color="primary"
+      size="64"
+      width="5"
+      indeterminate
+    />
+  </v-overlay>
   <v-container>
     <v-row no-gutters class="d-flex ga-4">
-      <v-col>
+      <v-col v-if="totalRedzones">
         <CardDashboard
           title="Redzones"
           description="Quantidade total de redzones"
           icon="mdi mdi-circle-off-outline"
-          result="64"
+          :result="totalRedzones"
           :date="new Date().toLocaleTimeString('pt-BR')"
         />
       </v-col>
@@ -19,33 +28,33 @@
           :date="new Date().toLocaleTimeString('pt-BR')"
         />
       </v-col>
-      <v-col>
+      <v-col v-if="totalUsers">
         <CardDashboard
           title="Usuários"
           description="Quantidade total de usuários"
           icon="fa-users"
-          result="18"
+          :result="totalUsers"
           :date="new Date().toLocaleTimeString('pt-BR')"
         />
       </v-col>
       <v-divider class="py-1 mt-2"></v-divider>
       <v-responsive width="100%"></v-responsive>
-      <v-col>
+      <v-col v-if="logService.dadosAlternativos">
         <CardDashboard
           title="Redzone com mais logs"
-          description="Redzone que mais tem logs registrados"
+          :description="logService.dadosAlternativos.qtde + ' logs registrados'"
           icon="mdi mdi-circle-off-outline"
-          result="Armazém"
+          :result="logService.dadosAlternativos.nome"
           sizeResult="text-h5"
           :date="new Date().toLocaleTimeString('pt-BR')"
         />
       </v-col>
-      <v-col>
+      <v-col v-if="userMaisRedzones[1]">
         <CardDashboard
           title="Usuário com mais redzones"
-          description="Usuário que mais tem redzones registrados"
+          :description="userMaisRedzones[1] + ' redzones'"
           icon="mdi mdi-account"
-          result="Marcelo"
+          :result="userMaisRedzones[0].nome_usuario"
           sizeResult="text-h5"
           :date="new Date().toLocaleTimeString('pt-BR')"
         />
@@ -73,33 +82,33 @@
     </v-row>
     <v-divider class="py-2 mt-5"></v-divider>
     <v-row>
-      <v-col>
+      <v-col v-if="seriesUserType" sm="5">
         <apexchart
           type="pie"
-          :options="chartOptions"
-          :series="series"
+          :options="chartOptionsUserType"
+          :series="seriesUserType"
         ></apexchart>
       </v-col>
-      <v-col>
+      <v-col v-if="seriesBarRedzonesDepartamentos" sm="7">
         <apexchart
           type="bar"
-          :options="chartOptionsBar"
-          :series="seriesBar"
+          :options="chartOptionsBarRedzonesDepartamentos"
+          :series="seriesBarRedzonesDepartamentos"
         ></apexchart>
       </v-col>
       <v-responsive width="100%"></v-responsive>
-      <v-col>
+      <v-col v-if="redzonesUser" sm="6">
         <apexchart
           type="bar"
-          :options="chartOptionsBar2"
-          :series="seriesBar2"
+          :options="chartOptionsBarRedzonesUser"
+          :series="seriesBarRedzonesUser"
         ></apexchart>
       </v-col>
-      <v-col>
+      <v-col sm="6">
         <apexchart
           type="bar"
-          :options="chartOptionsBar3"
-          :series="seriesBar3"
+          :options="chartOptionsBarrr"
+          :series="seriesBarrr"
         ></apexchart>
       </v-col>
     </v-row>
@@ -114,8 +123,8 @@
       <v-col>
         <apexchart
           type="line"
-          :options="chartOptionsLine2"
-          :series="seriesLine2"
+          :options="chartOptionsLinee"
+          :series="seriesLinee"
         ></apexchart>
       </v-col>
     </v-row>
@@ -123,38 +132,12 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import CardDashboard from "../../components/dashboard/cardDashboard.vue";
+import { RedzoneStore, registroRedzoneStore, UsuarioStore } from "../../stores";
+import useNotification from "../../stores/notification";
 
-const chartOptions = reactive({
-  chart: {
-    type: "pie",
-  },
-  title: {
-    text: "Tipos de usuários",
-    style: {
-      fontSize: "18px",
-    },
-  },
-  labels: ["Adm Geral", "Gerente de área", "Guarda"],
-  responsive: [
-    {
-      breakpoint: 300,
-      options: {
-        chart: {
-          width: 200,
-        },
-        legend: {
-          position: "bottom",
-        },
-      },
-    },
-  ],
-});
-
-const series = [2, 5, 13];
-
-const chartOptionsBar = reactive({
+const chartOptionsBarRedzonesDepartamentos = reactive({
   chart: {
     type: "bar",
   },
@@ -178,7 +161,7 @@ const chartOptionsBar = reactive({
     title: {
       text: "Departamentos",
     },
-    categories: ["Cozinha", "Armazém", "Escritório", "Manutenção"],
+    categories: [],
   },
   yaxis: {
     title: {
@@ -190,14 +173,58 @@ const chartOptionsBar = reactive({
   },
 });
 
-const seriesBar = [
+const seriesBarRedzonesDepartamentos = [
   {
     name: "Redzones",
-    data: [23, 18, 13, 10],
+    data: [],
   },
 ];
 
-const chartOptionsBar2 = reactive({
+const chartOptionsBarRedzonesUser = reactive({
+  chart: {
+    type: "bar",
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "55%",
+      endingShape: "rounded",
+    },
+  },
+  dataLabels: {
+    enabled: true,
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ["transparent"],
+  },
+  xaxis: {
+    title: {
+      text: "Usuários",
+    },
+    categories: [],
+  },
+  yaxis: {
+    title: {
+      text: "Qtde de Redzones",
+    },
+  },
+  fill: {
+    opacity: 1,
+  },
+  colors: "#16a34a",
+});
+
+const seriesBarRedzonesUser = reactive([
+  {
+    name: "Redzones",
+    data: [],
+  },
+]);
+
+const chartOptionsBarrr = reactive({
   chart: {
     type: "bar",
   },
@@ -225,50 +252,6 @@ const chartOptionsBar2 = reactive({
   },
   yaxis: {
     title: {
-      text: "Qtde de Redzones",
-    },
-  },
-  fill: {
-    opacity: 1,
-  },
-  colors: "#16a34a",
-});
-
-const seriesBar2 = [
-  {
-    name: "Redzones",
-    data: [23, 18, 13, 10],
-  },
-];
-
-const chartOptionsBar3 = reactive({
-  chart: {
-    type: "bar",
-  },
-  plotOptions: {
-    bar: {
-      borderRadius: 4,
-      horizontal: false,
-      columnWidth: "55%",
-      endingShape: "rounded",
-    },
-  },
-  dataLabels: {
-    enabled: true,
-  },
-  stroke: {
-    show: true,
-    width: 2,
-    colors: ["transparent"],
-  },
-  xaxis: {
-    title: {
-      text: "Qtde Redzones",
-    },
-    categories: ["Marcelo", "Gabriel", "Everton", "Dionísio"],
-  },
-  yaxis: {
-    title: {
       text: "Qtde Departamentos",
     },
   },
@@ -278,7 +261,7 @@ const chartOptionsBar3 = reactive({
   colors: "#ef4444",
 });
 
-const seriesBar3 = [
+const seriesBarrr = [
   {
     name: "Redzones",
     data: [23, 18, 13, 10],
@@ -330,7 +313,7 @@ const chartOptionsLine = reactive({
   colors: ["#082f49"],
 });
 
-const seriesLine2 = [
+const seriesLinee = [
   {
     name: "Logs de Entrada",
     data: [10, 20, 15, 30, 25, 35, 40],
@@ -341,7 +324,7 @@ const seriesLine2 = [
   },
 ];
 
-const chartOptionsLine2 = reactive({
+const chartOptionsLinee = reactive({
   chart: {
     type: "line",
     height: 350,
@@ -356,7 +339,7 @@ const chartOptionsLine2 = reactive({
     curve: "smooth",
   },
   title: {
-    text: "Quantidade de Logs de Entrada e Saída de uma Redzone nos Últimos 7 Dias",
+    text: "Qtde de Logs de Entrada/Saída de uma Redzone nos Últimos 7 Dias",
     align: "left",
   },
   grid: {
@@ -382,5 +365,113 @@ const chartOptionsLine2 = reactive({
       formatter: (val: number) => `${val} logs`,
     },
   },
+});
+
+const notificator = useNotification();
+const redzoneService = RedzoneStore();
+const usuarioService = UsuarioStore();
+const logService = registroRedzoneStore();
+
+const totalRedzones = ref("");
+const totalUsers = ref("");
+const userMaisRedzones = ref({});
+const usersByType = ref();
+const redzonesDepartamento = ref();
+const redzonesUser = ref();
+const loading = ref(false);
+
+function adaptData(data) {
+  const totals = {
+    "Adm Geral": 0,
+    "Gerente de área": 0,
+    Guarda: 0,
+  };
+
+  const labelMap = {
+    ROLE_MANAGER: "Gerente de área",
+    Administrador: "Adm Geral",
+    "Gerente de área": "Gerente de área",
+    ROLE_ADMIN: "Adm Geral",
+    ROLE_GUARD: "Guarda",
+    Guarda: "Guarda",
+  };
+
+  data.forEach((item) => {
+    if (Array.isArray(item) && item.length === 2) {
+      const [role, count] = item;
+      const label = labelMap[role];
+      if (label) {
+        totals[label] += count;
+      }
+    }
+  });
+
+  return [totals["Adm Geral"], totals["Gerente de área"], totals["Guarda"]];
+}
+
+const chartOptionsUserType = reactive({
+  chart: {
+    type: "pie",
+  },
+  title: {
+    text: "Tipos de usuários",
+    style: {
+      fontSize: "18px",
+    },
+  },
+  labels: ["Adm Geral", "Gerente de área", "Guarda"],
+  responsive: [
+    {
+      breakpoint: 300,
+      options: {
+        chart: {
+          width: 200,
+        },
+        legend: {
+          position: "bottom",
+        },
+      },
+    },
+  ],
+});
+
+const seriesUserType = ref();
+
+onMounted(async () => {
+  loading.value = true;
+  try {
+    totalRedzones.value = (await redzoneService.getTotalRedzones()).data;
+    //
+    totalUsers.value = (await usuarioService.getTotalUsuarios()).data;
+    //
+    userMaisRedzones.value = (
+      await usuarioService.getUserMaisRedzones()
+    ).data[0];
+    //
+    usersByType.value = (await usuarioService.getUsersByType()).data;
+    seriesUserType.value = adaptData(usersByType.value);
+    //
+    redzonesDepartamento.value = (
+      await redzoneService.getRedzonesByDepartamento()
+    ).data;
+    redzonesDepartamento.value.forEach((item) => {
+      const [label, qtde] = item;
+      chartOptionsBarRedzonesDepartamentos.xaxis.categories.push(label);
+      seriesBarRedzonesDepartamentos[0].data.push(qtde);
+    });
+    //
+    redzonesUser.value = (await redzoneService.getRedzonesByUser()).data;
+    redzonesUser.value.forEach((item) => {
+      const [label, qtde] = item;
+      chartOptionsBarRedzonesUser.xaxis.categories.push(label);
+      seriesBarRedzonesUser[0].data.push(qtde);
+    });
+    //
+    logService.getRedzoneMaisLog();
+  } catch (error) {
+    notificator.notifyError(error.response.data);
+  } finally {
+    loading.value = false;
+  }
 });
 </script>
