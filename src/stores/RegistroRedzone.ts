09fa8c4e3ba5a51b8c199a2705680d1f98dfcc80
Bruna <brunadias_30@hotmail.com>
2024-06-03 20:21:28 -0300

    feature: criando endpoints de logs

--- src/stores/RegistroRedzone.ts
+++ src/stores/RegistroRedzone.ts
@@ -4,25 +4,35 @@ import { ref } from "vue";
 import { IregistroRedzone } from "../interfaces/IRedzoneRegistro";
 
 const registroRedzoneStore = defineStore('registro', () => {
-    const dadosRedzone = ref<IregistroRedzone[]>([])
+  const dadosRedzone = ref<IregistroRedzone[]>([])
+  const dadosAlternativos = ref()
 
-    const pegarHistorico = async () => {
-        const response = await getRequest('log')
+  const pegarHistorico = async () => {
+    const response = await getRequest('log')
 
-        dadosRedzone.value = response.data.reverse();
-    }
+    dadosRedzone.value = response.data.reverse();
+  }
 
-    const pegarHistoricoRedZone = async (idRedzone: any) => {
-      const response = await getRequest(`log/redzone/${idRedzone}`)
-      dadosRedzone.value = response.data.reverse();
+  const pegarHistoricoRedZone = async (idRedzone: any) => {
+    const response = await getRequest(`log/redzone/${idRedzone}`)
+    dadosRedzone.value = response.data.reverse();
+  }
+
+  const getRedzoneMaisLog = async () => {
+    const response = await getRequest(`log/redzonemaislogs`)
+    const nome = response.data[0][0].nome_redzone
+    const qtde = response.data[0][1]
+    dadosAlternativos.value = { nome: nome, qtde: qtde }
   }
 
 
-    return {
-        dadosRedzone,
-        pegarHistorico,
-        pegarHistoricoRedZone
-    }
+  return {
+    dadosRedzone,
+    dadosAlternativos,
+    pegarHistorico,
+    pegarHistoricoRedZone,
+    getRedzoneMaisLog
+  }
 })
 
 export default registroRedzoneStore;
