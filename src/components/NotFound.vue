72d510b2fea2016b22cfdf8c2f36177c04559d06
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-06-11 07:56:48 -0300

    feature: Criando componente de notFound

---
+++ src/components/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="d-flex align-center justify-center fill-height" fluid>
+    <v-row class="text-center mt-16">
+      <v-col cols="12">
+        <h2 class="subtitle-1 "><v-icon icon="mdi mdi-exclamation"/>git checkout 153-criar-página-de-not-foundPágina não encontrada</h2>
+        <v-btn color="primary" @click="goHome">Voltar</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goHome = () => {
+  router.back()
+};
+</script>
+
